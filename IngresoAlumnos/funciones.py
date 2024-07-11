ab548b68d203eea86605803c6a2349245417cd5f
RETR0NACH <167935128+RETR0NACH@users.noreply.github.com>
2024-07-11 11:13:40 -0400

    Update funciones.pyexamen

--- IngresoAlumnos/funciones.py
+++ IngresoAlumnos/funciones.py
@@ -1,59 +1,68 @@
+import random
+import csv
+trabajadores = [
+    {"nombre": "Juan Pérez",},
+    {"nombre": "María García", },
+    {"nombre": "Carlos López", },
+    {"nombre": "Ana Martínez", },
+    {"nombre": "Pedro Rodríguez", },
+    {"nombre": "Laura Hernández", },
+    {"nombre": "Miguel Sánchez", },
+    {"nombre": "Isabel Gómez", },
+    {"nombre": "Francisco Díaz", },
+    {"nombre": "Elena Fernández",}
+]
 
-curso = []
-alumnos = []
-def agregarAlumnos(coleccion):
+import csv
+import math 
+# Asignación de sueldos aleatorios
+sueldos = []
 
-    nombre = input("Ingrese el nómbre del alumno--->")
-    apellido = input("Ingrese el apellido del alumno--->")
-    nivel  = input("Ingrese el curso del alumno--->")
-    promedio = input("Ingrese el promedio del alumno--->")
+def asignar_sueldos_aleatorios():
+    global sueldos #solo usamos el sueldo dentro de que está en este def
+    sueldos = [random.randint(300000, 2500000) for _ in range(10)]
+    print("Sueldos asignados aleatoriamente.")
 
-    coleccion.append([nombre, apellido, nivel, promedio])
+def clasificar_sueldos(): #Se asignan los sueldos < $80000
+    print("Sueldos menores a $800.000 TOTAL:", len([s for s in sueldos if s < 800000]))
+    for trabajador, sueldo in zip(trabajadores, sueldos):
+        if sueldo < 800000:
+            print(f"Nombre empleado: {trabajador['nombre']}  Sueldo: ${sueldo}")
+    #Asignamos los sueldos que sean entre $800.000 y $2.000.000
+    print("\nSueldos entre $800.000 y $2.000.000 TOTAL:", len([s for s in sueldos if 800000 <= s <= 2000000]))
+    for trabajador, sueldo in zip(trabajadores, sueldos):
+        if 800000 <= sueldo <= 2000000:
+            print(f"Nombre empleado: {trabajador['nombre']}  Sueldo: ${sueldo}")
+    #Asignamos sueldos mayores a 2.000.000
+    print("\nSueldos superiores a $2.000.000 TOTAL:", len([s for s in sueldos if s > 2000000]))
+    for trabajador, sueldo in zip(trabajadores, sueldos):
+        if sueldo > 2000000:
+            print(f"Nombre empleado: {trabajador['nombre']}  Sueldo: ${sueldo}")
+    #suma total de los sueldos de los empleados
+    print("\nTOTAL SUELDOS:", sum(sueldos))
 
-    print("Se ha ingresado correctamente el alumno :)")
-    for estudiante in coleccion:
-        print(estudiante)
+def ver_estadisticas():
+    sueldo_max = max(sueldos) #sueldo maximo que tuvo un cliente
+    sueldo_min = min(sueldos) #sueldo minimo del cliente
+    sueldo_promedio = sum(sueldos) / len(sueldos) #promedio de sueldo maximo y mínimo
 
-def mostrarCurso(curso):
-    contador = 1
-    print(" NOMBRE | APELLIDO |  CURSO | PROMEDIO ")
-    for alumnos in curso:
-        print(f"{contador}.-{alumnos[0]} | {alumnos[1]} | {alumnos[2]} | {alumnos[3]}")
-        contador = contador + 1
 
-def eliminarAlumno(coleccion):
-    try:
-        alumnoEliminar = input("Ingrese el nombre del alumno que desea eliminar: ")
-    except ValueError:
-        print("ERR0r.. vuelva a intenar")
+    print(f"Sueldo más alto: ${sueldo_max}")
+    print(f"Sueldo más bajo: ${sueldo_min}")
+    print(f"Promedio de sueldos: ${sueldo_promedio:.2f}")
 
-    for estudiante in coleccion:
-        if estudiante[0] == alumnoEliminar:
-            coleccion.remove(estudiante)
-            print(f"El alumno {alumnoEliminar} ha sido eliminado correctamente.")
-            break
-    else:
-        print(f"No se encontró al alumno {alumnoEliminar}.")
 
-def guardarAlumno(curso):
-    # Creación de archivo TXT
-    with open('AlumnosIngresados.txt', 'w', encoding='utf-8', newline='') as archivo:
-        archivo.write(f"{'| NOMBRE |': <20}{'| APELLIDO |':<20}{'| CURSO |': <20}{'| PROMEDIO |':<20}\n")  # Agregamos \n al final
-        archivo.write("-" * 100 + "\n")  # Línea de separación 
-        for alumnos in curso:
-            archivo.write(f"{alumnos[0]: <20}{alumnos[1]: <20}{alumnos[2]: <20}{alumnos[3]: <20}\n")  # Agregamos \n al final
+def reporte_sueldos():
+    with open('reporte_sueldos.csv', 'w', encoding='utf-8' ,newline='') as archivo:
+        archivo = csv.writer(archivo)
+        archivo.writerow(["NOMBRE EMPLEADO", "SUELDO BASE", "DESCUENTO SALUD", "DESCUENTO AFP", "SUELDO LIQUIDO"])
+        for trabajador, sueldo in zip(trabajadores, sueldos):
+            descuento_salud = sueldo * 0.07
+            descuento_afp = sueldo * 0.12
+            sueldo_liquido = sueldo - descuento_salud - descuento_afp
+            archivo.writerow([trabajador["nombre"], trabajador, sueldo, descuento_salud, descuento_afp, sueldo_liquido])
+            print(f"NOMBRE EMPLEADO: {trabajador['nombre']}  SUELDO BASE: ${sueldo} DESCUENTO SALUD: ${descuento_salud:.2f} DESCUENTO AFP: ${descuento_afp:.2f} SUELDO LIQUIDO: ${sueldo_liquido:.2f}")
 
-def modificarEstudiante():
-    mostrarCurso()
-    posicion = int(input("Ingresa el número que quieras modificar--->"))
-    print(f"El curso para modificar un estudiante es {curso[posicion-1]}")
-    try:
-        nombreNuevo = input("Ingresa el nómbre del estudiante--->")
-        apellidoNuevo = input("Ingresa el apellido del estudiante--->")
-        cursoNuevo = input("Ingresa el curso del estudiante--->")
-        promedioNuevo = input("Ingresa el promedio del estudiante--->")
-    except ValueError:
-        print("ERR0R=!#... Intentelo de nuevo.")
 
     curso[posicion-1] = [nombreNuevo, apellidoNuevo, cursoNuevo, promedioNuevo]
     print("Se modificó el estudiante correctamente.")
